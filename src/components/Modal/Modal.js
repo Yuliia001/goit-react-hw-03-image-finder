15bb76a63017fcf7f5c16b86bf76f89e6951ed64
Yuliia001 <babeyuliia@gmail.com>
2023-09-04 19:40:14 +0200

    add modal change

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react';
 import { Overlay, ModalWindow } from './Modal.styled';
 
 export class Modal extends Component {
+  componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
+  }
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.props.closeModal();
+    }
+  };
   render() {
     const { selectedImage, closeModal, tags } = this.props;
     return (
