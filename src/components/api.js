6052dbf0170b4e9ad0f4173920c352b4464a615b
Yuliia001 <babeyuliia@gmail.com>
2023-09-06 20:56:48 +0200

    fix api.js

--- src/components/api.js
+++ src/components/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://pixabay.com/api/';
+axios.defaults.baseURL = 'https://pixabay.com/api';
 
 const KEY = '38369214-2131a54870ec208cdae419196';
 
@@ -13,6 +13,6 @@ export const dataQuery = async (query, page = 1) => {
     per_page: 12,
     page: page,
   };
-  const responce = await axios.get(`?`, { params });
+  const responce = await axios.get(`/`, { params });
   return responce.data;
 };
